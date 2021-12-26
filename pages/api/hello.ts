0f8e99af98c5e1af2298e9b53752b62fded0a73a
Leonardo Zivieri <leonardozivieri150@gmail.com>
2021-12-25 21:26:03 -0300

    Add Typescript

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import { NextApiHandler } from "next"
+
+const apiHello: NextApiHandler = function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+export default apiHello;
